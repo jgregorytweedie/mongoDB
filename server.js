9f4dd8e00d2e9d71c323d1f57c216d15ee6a9b8f
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-09 10:48:37 -0700

    required dependencies

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URL, { useUnifiedTopology: true,useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once('open', function() {
+  console.log('Connected to DB...');
+});
