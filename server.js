94fe0515c678d6f71f9ef66a245c00ab2c9e51f0
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-09 11:22:13 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://pogchamp:pogchamp@cluster0.9jipw.mongodb.net/webdev?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 mongoose.connect(process.env.MONGODGB_URL, { UseUnifiedTopology: true,useNewUrlParser: true});
 
